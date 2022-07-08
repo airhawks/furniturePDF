c29f19f4b5809daf8533d840979a518b3ca3905e
Amit Dhaka <amitdhakaer@gmail.com>
2022-07-08 09:15:55 -0700

    parse furniture json

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./styles.css";
+import furnitureList from "./furnitureList.js";
+
+const json = furnitureList.split("\n");
+const keys = json[0].split("\t");
+
+const list = json.slice(1).map((row) => {
+  const rowJson = row.split("\t");
+  return keys.reduce((obj, key, index) => {
+    const value = rowJson[index].split(" | ");
+    obj[key] = value.length === 1 ? value[0] : value;
+    return obj;
+  }, {});
+});
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Kaushal Homes</h1>
+      {list.map((item, index) => (
+        <h4 key={index}> {item.dimensions} </h4>
+      ))}
     </div>
   );
 }
