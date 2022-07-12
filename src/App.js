e437f794cbda1137c855f41a5a5ed3449da2584e
Amit Dhaka <amitdhakaer@gmail.com>
2022-07-11 19:55:56 -0700

    Completed Page 2

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import FurnitureItem from "./FurnitureItem";
 export default function App() {
   return (
     <div className="App">
-      {/* <h1>Kaushal Homes</h1> */}
-      {furnitureList.map((item, index) => (
+      {furnitureList.slice(0, 10).map((item, index) => (
         <FurnitureItem item={item} key={index} />
       ))}
     </div>
--- src/FurnitureItem.js
+++ src/FurnitureItem.js
@@ -1,5 +1,88 @@
 import "./styles.css";
 
 export default function FurnitureItem({ item }) {
-  return <h4> {item.dimensions} </h4>;
+  const firstPageBottomImages = item.img.slice(2, 4);
+  const secondPageMiddleImages = item.img.slice(4, 6);
+  const secondPageTopImage = item.img[6];
+  const secondPageBottomImage = item.img[1];
+  const dimensionImage = item.img[7];
+  const finishDetails = item.description[2].split(":");
+  return (
+    <div className="FurnitureItem">
+      <div className="FurnitureItem_ViewPortPage">
+        <img
+          className="FurnitureItem_MainImage"
+          src={item.img[0]}
+          alt="main"
+        ></img>
+
+        <div className="FurnitureItem_Details">
+          <div className="FurnitureItem_Line" />
+          <div className="FurnitureItem_Title"> {item.title} </div>
+          <div className="FurnitureItem_Price"> {item.price} </div>
+
+          <div className="FurnitureItem_Description">
+            {" "}
+            {item.description[0]}{" "}
+          </div>
+
+          <div className="FurnitureItem_PageBottom">
+            {firstPageBottomImages.map((src, index) => (
+              <img
+                key={index}
+                className="FurnitureItem_BottomImage"
+                src={src}
+                alt="bottom page"
+              ></img>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="FurnitureItem_ViewPortPage">
+        <div className="FurnitureItem-Page2">
+          <div className="FurnitureItem_Line FurnitureItem-Page2_Line" />
+
+          <img
+            className="FurnitureItem-Page2_TopImage"
+            src={secondPageTopImage}
+            alt="bottom page"
+          ></img>
+
+          <div className="FurnitureItem-Page2_MiddleImagesContainer">
+            {secondPageMiddleImages.map((src, index) => (
+              <img
+                key={index}
+                className="FurnitureItem-Page2_MiddleImages"
+                src={src}
+                alt="bottom page"
+              ></img>
+            ))}
+          </div>
+
+          <img
+            className="FurnitureItem-Page2_BottomImage"
+            src={secondPageBottomImage}
+            alt="bottom page"
+          ></img>
+
+          <div className="FurnitureItem-Page2_RowFlexBox">
+            <img
+              className="FurnitureItem-Page2_DimensionImage"
+              src={dimensionImage}
+              alt="bottom page"
+            ></img>
+            <div className="FurnitureItem_Line FurnitureItem-Page2_VerticalLine" />
+            <div className="FurnitureItem-Page2_Dimensions">
+              <h2> Dimensions </h2>
+              {item.dimensions.join(" | ")}
+              <div className="FurnitureItem-Page2_Finish">
+                <h3>{finishDetails[0] + ":"}</h3>
+                {finishDetails[1]}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
